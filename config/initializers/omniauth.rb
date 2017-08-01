4b6148d251fc60d98542cb6ff497874f0f4aa20e
Matheus Carmo <matheus.carmo@resultadosdigitais.com.br>
2017-08-01 15:42:19 -0300

    Disable JWT checker at omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,8 @@
 #Omniath
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, Figaro.env.google_client_id, Figaro.env.google_client_secret
+  provider :google_oauth2, 
+           Figaro.env.google_client_id, 
+           Figaro.env.google_client_secret,
+           verify_iss: false
 end
