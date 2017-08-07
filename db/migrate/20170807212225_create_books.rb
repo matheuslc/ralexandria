43effd83454392d24d3ccfe39e63c4e09f6271d4
Matheus Carmo <matheus.carmo@resultadosdigitais.com.br>
2017-08-07 18:22:50 -0300

    Add Book model

---
+++ db/migrate/20170807212225_create_books.rb
@@ -0,0 +1,16 @@
+class CreateBooks < ActiveRecord::Migration[5.1]
+  def change
+    create_table :books do |t|
+      t.string :title
+      t.string :author
+      t.string :external_link
+      t.string :authors
+      t.string :publisher
+      t.string :description
+      t.int :pages
+      t.string :language
+
+      t.timestamps
+    end
+  end
+end
